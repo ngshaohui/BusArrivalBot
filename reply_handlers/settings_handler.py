3490a98f87b4afeafbb6a8c95aefa3f354da2855
unknown <ngshaohui8@gmail.com>
2025-09-08 17:00:25 +0800

    add reorder logic

--- reply_handlers/settings_handler.py
+++ reply_handlers/settings_handler.py
@@ -22,11 +22,11 @@ SETTINGS_KEYBOARD = [
             "Remove stops", callback_data=SETTINGS_ACTIONS.REMOVE_FLOW.value
         )
     ],
-    # [
-    #     InlineKeyboardButton(
-    #         "Reorder stops", callback_data=SETTINGS_ACTIONS.REORDER.value
-    #     )
-    # ],
+    [
+        InlineKeyboardButton(
+            "Reorder stops", callback_data=SETTINGS_ACTIONS.REORDER_FLOW.value
+        )
+    ],
     [
         InlineKeyboardButton(
             "Revoke permissions", callback_data=SETTINGS_ACTIONS.DECLINE_FLOW.value
@@ -139,7 +139,7 @@ async def ask_consent(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     """
     ask user for consent to save settings
     """
-    text = """No user data found.
+    text = """No existing user data found.
 
 Allow the bot to store your configuration settings?"""
     reply_markup = InlineKeyboardMarkup(SETTINGS_CONSENT_KEYBOARD)
@@ -206,8 +206,10 @@ async def add_stop(
     )
 
 
-def __make_saved_stops_removal_list(
-    get_stop_info: GetStopInfo, saved_stops: list[str]
+def __make_saved_stops_list(
+    get_stop_info: GetStopInfo,
+    saved_stops: list[str],
+    settings_action: SETTINGS_ACTIONS,
 ) -> list[list[InlineKeyboardButton]]:
     """
     TODO describe
@@ -218,7 +220,7 @@ def __make_saved_stops_removal_list(
         if stop_info is not None:
             button = InlineKeyboardButton(
                 f"{stop_info['BusStopCode']} | {stop_info['Description']}",
-                callback_data=f"{SETTINGS_ACTIONS.REMOVE.value},{stop_info['BusStopCode']}",
+                callback_data=f"{settings_action.value},{stop_info['BusStopCode']}",
             )
             buttons.append([button])
     return buttons
@@ -246,8 +248,8 @@ def remove_flow_handler(
             text = "Remove a stop from the list below:"
         else:
             text = "List is empty."
-        callback_buttons = __make_saved_stops_removal_list(
-            get_stop_info, saved_stops
+        callback_buttons = __make_saved_stops_list(
+            get_stop_info, saved_stops, SETTINGS_ACTIONS.REMOVE
         ) + [BACK_TO_SETTINGS_BUTTON]
         reply_markup = InlineKeyboardMarkup(callback_buttons)
         await query.answer()
@@ -280,6 +282,162 @@ def remove_handler(
         await remove_flow(update, context)
 
     return remove_stop
+
+
+def reorder_flow_handler(
+    storage_utility: StorageUtility, get_stop_info: GetStopInfo
+) -> Callable:
+    async def reorder_flow(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """
+        TODO handle scenario where stops exist or don't exist already
+        """
+        query = update.callback_query
+        chat_id = get_chat_id(update, context)
+        if query is None or query.data is None or chat_id is None:
+            return  # ignore malformed requests
+
+        user_exists = storage_utility.check_user_exists(chat_id)
+        if not user_exists:
+            return await __show_info(update)
+
+        saved_stops = storage_utility.get_saved_stops(chat_id)
+        text = "Select a stop from the list below:"
+        callback_buttons = __make_saved_stops_list(
+            get_stop_info, saved_stops, SETTINGS_ACTIONS.REORDER_SELECT
+        ) + [BACK_TO_SETTINGS_BUTTON]
+        reply_markup = InlineKeyboardMarkup(callback_buttons)
+        await query.answer()
+        await query.edit_message_text(text=text, reply_markup=reply_markup)
+
+    return reorder_flow
+
+
+def __make_reorder_keyboard(stop_id: str, position: int) -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "Up",
+                    callback_data=f"{SETTINGS_ACTIONS.REORDER.value},{stop_id},{position},{0}",
+                ),
+                InlineKeyboardButton(
+                    "Down",
+                    callback_data=f"{SETTINGS_ACTIONS.REORDER.value},{stop_id},{position},{1}",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    "Done", callback_data=SETTINGS_ACTIONS.REORDER_FLOW.value
+                ),
+            ],
+        ]
+    )
+
+
+def __get_reorder_list_message(
+    get_stop_info: GetStopInfo, saved_stops: list[str], selected_pos: int
+) -> str:
+    """
+    Displays the order of the current list of saved stops
+
+    Uses 1 based indexing
+    """
+    msg = ""
+    # stop_info_ls = [info for stop in saved_stops if (info := get_stop_info(stop)) is not None]
+    stop_info_iter = map(get_stop_info, saved_stops)
+    for idx, stop_info in enumerate(stop_info_iter):
+        if stop_info is not None:
+            idx_marker = f"> {idx + 1}." if idx == selected_pos else f"{idx + 1} ."
+            msg += f"{idx_marker} {stop_info['BusStopCode']} | {stop_info['Description']}\n"
+    return msg.strip()
+
+
+def reorder_select_handler(
+    storage_utility: StorageUtility, get_stop_info: GetStopInfo
+) -> Callable:
+    async def reorder_select(
+        update: Update, context: ContextTypes.DEFAULT_TYPE
+    ) -> None:
+        """
+        TODO handle scenario where stops exist or don't exist already
+        """
+        query = update.callback_query
+        chat_id = context._chat_id
+        if query is None or query.data is None or chat_id is None:
+            return  # ignore malformed requests
+
+        user_exists = storage_utility.check_user_exists(chat_id)
+        if not user_exists:
+            return await __show_info(update)
+
+        selected_stop_id = query.data.split(",")[1]
+
+        saved_stops = storage_utility.get_saved_stops(chat_id)
+        try:
+            idx = saved_stops.index(selected_stop_id)
+            text = __get_reorder_list_message(get_stop_info, saved_stops, idx)
+            reply_markup = __make_reorder_keyboard(selected_stop_id, idx)
+            await query.answer()
+            await query.edit_message_text(text, reply_markup=reply_markup)
+
+        except ValueError:
+            text = "Data for this message is outdated. Please use /settings for the latest data."
+            await query.answer()
+            await query.edit_message_text(text)
+
+    return reorder_select
+
+
+def __reorder_stops_list(saved_stops: list[str], pos: int, dir: str) -> list[str]:
+    ls = saved_stops[::]
+    target = saved_stops[pos]
+    if dir == "0":  # move forward
+        if pos == 0:
+            # already first item
+            return ls
+        ls[pos] = ls[pos - 1]
+        ls[pos - 1] = target
+    else:  # move backwards
+        if pos == len(saved_stops) - 1:
+            # already last item
+            return ls
+        ls[pos] = ls[pos + 1]
+        ls[pos + 1] = target
+    return ls
+
+
+def reorder_handler(
+    storage_utility: StorageUtility, get_stop_info: GetStopInfo
+) -> Callable:
+    async def reorder_stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """
+        TODO handle scenario where stops exist or don't exist already
+        """
+        query = update.callback_query
+        chat_id = context._chat_id
+        if query is None or query.data is None or chat_id is None:
+            # ignore malformed requests
+            return
+
+        user_exists = storage_utility.check_user_exists(chat_id)
+        if not user_exists:
+            return await __show_info(update)
+
+        _, stop_id, position, direction = query.data.split(",")
+        saved_stops = storage_utility.get_saved_stops(chat_id)
+        # validate the stop exists at that position
+        if saved_stops[int(position)] != stop_id:
+            # TODO show error about invalid data
+            return
+        new_stops_order = __reorder_stops_list(saved_stops, int(position), direction)
+        storage_utility.save_stops(chat_id, new_stops_order)
+
+        # TODO implement flow for finish reorder
+
+        reorder_select = reorder_select_handler(storage_utility, get_stop_info)
+        await reorder_select(update, context)
+
+    return reorder_stop
 
 
 def register_settings_handlers(
@@ -324,5 +482,23 @@ def register_settings_handlers(
         CallbackQueryHandler(
             remove_handler(storage_utility, service_integrator.get_stop_info),
             pattern=rf"{SETTINGS_ACTIONS.REMOVE.value},\d{{5}}",
+        )
+    )
+    application.add_handler(
+        CallbackQueryHandler(
+            reorder_flow_handler(storage_utility, service_integrator.get_stop_info),
+            pattern=SETTINGS_ACTIONS.REORDER_FLOW.value,
+        )
+    )
+    application.add_handler(
+        CallbackQueryHandler(
+            reorder_select_handler(storage_utility, service_integrator.get_stop_info),
+            pattern=rf"{SETTINGS_ACTIONS.REORDER_SELECT.value},\d{{5}}",
+        )
+    )
+    application.add_handler(
+        CallbackQueryHandler(
+            reorder_handler(storage_utility, service_integrator.get_stop_info),
+            pattern=rf"{SETTINGS_ACTIONS.REORDER.value},\d{{5}},\d,[01]",
         )
     )
