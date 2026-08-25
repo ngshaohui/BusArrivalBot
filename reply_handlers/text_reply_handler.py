4a21fb06fd07df073a2d0c5b612214a737efc286
shaohui <shinlos@hotmail.com>
2026-08-25 22:25:10 +0800

    simplify message passing for text reply handler

--- reply_handlers/text_reply_handler.py
+++ reply_handlers/text_reply_handler.py
@@ -1,7 +1,7 @@
 import re
 import time
 
-from telegram import InlineKeyboardMarkup, Update
+from telegram import InlineKeyboardMarkup, Message, Update
 from telegram.ext import ContextTypes
 
 from bot_app_state import get_app_state
@@ -42,26 +42,27 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
     this function then sends the request to the specific handler
     """
-    if update.message is None or update.message.text is None:
+    message = update.message
+    if message is None or message.text is None:
         return
-    msg = update.message.text
+    msg_text = message.text
     app_state = get_app_state(context.application)
 
-    if match := re.match(REGEX_STOP_CODE, msg):
+    if match := re.match(REGEX_STOP_CODE, msg_text):
         stop_code = match.group(1)
-        await bus_stop_code(app_state.bus_service.get_stop_info, update, stop_code)
-    elif match := re.match(REGEX_BUS_NUM, msg, re.IGNORECASE):
+        await bus_stop_code(app_state.bus_service.get_stop_info, message, stop_code)
+    elif match := re.match(REGEX_BUS_NUM, msg_text, re.IGNORECASE):
         bus_number = match.group(1)
-        await bus_route(app_state.bus_service.get_route_stops, update, bus_number)
-    elif match := re.match(REGEX_ROUTE, msg, re.IGNORECASE):
-        # TODO can deprecate this command
+        await bus_route(app_state.bus_service.get_route_stops, message, bus_number)
+    elif match := re.match(REGEX_ROUTE, msg_text, re.IGNORECASE):
+        # TODO consider deprecating this command
         bus_number = match.group(1)
-        await bus_route(app_state.bus_service.get_route_stops, update, bus_number)
-    elif match := re.match(REGEX_SEARCH, msg, re.IGNORECASE):
+        await bus_route(app_state.bus_service.get_route_stops, message, bus_number)
+    elif match := re.match(REGEX_SEARCH, msg_text, re.IGNORECASE):
         query_str = match.group(1)
         query: list[str] = re.split(r"[\s\/\-]", query_str)
-        await search(app_state.bus_service.search_possible_stops, update, query)
-    elif match := re.match(REGEX_ADD_STOP, msg, re.IGNORECASE):
+        await search(app_state.bus_service.search_possible_stops, message, query)
+    elif match := re.match(REGEX_ADD_STOP, msg_text, re.IGNORECASE):
         stop_code = match.group(1)
         await save_stop(
             app_state.storage_utility,
@@ -69,32 +70,29 @@ async def message_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
             update,
             stop_code,
         )
-    elif re.match(REGEX_LIST_SAVED_STOPS, msg, re.IGNORECASE) is not None:
+    elif re.match(REGEX_LIST_SAVED_STOPS, msg_text, re.IGNORECASE) is not None:
         await list_saved_stops(
             app_state.storage_utility, app_state.bus_service.get_stop_info, update
         )
     else:
         # unknown command message
-        await unknown_command(update)
+        await unknown_command(message)
 
 
 async def bus_stop_code(
-    get_stop_info: GetStopInfo, update: Update, stop_id: str
+    get_stop_info: GetStopInfo, message: Message, stop_id: str
 ) -> None:
     """
     reply user with bus arrival information
     """
-    if update.message is None:
-        return
-
     # craft message
     stop_info = get_stop_info(stop_id)
     if stop_info is None:
-        await update.message.reply_text("Unknown bus stop code")
+        await message.reply_text("Unknown bus stop code")
         return
     busses = get_arriving_busses(stop_id)
     if busses is None:
-        await update.message.reply_text(
+        await message.reply_text(
             "Currently experiencing issues with LTA's API, please try again later"
         )
         return
@@ -103,51 +101,44 @@ async def bus_stop_code(
     # refresh button
     reply_markup = InlineKeyboardMarkup(make_refresh_button(stop_id))
 
-    await update.message.reply_text(text=reply_msg, reply_markup=reply_markup)
+    await message.reply_text(text=reply_msg, reply_markup=reply_markup)
 
 
 async def bus_route(
-    get_route_stops: GetRouteStops, update: Update, bus_number: str | None
+    get_route_stops: GetRouteStops, message: Message, bus_number: str | None
 ) -> None:
     """
     reply user with bus route information
     """
-    if update.message is None:
-        return
-
     if bus_number is None:
-        await update.message.reply_text("Please provide a bus number")
+        await message.reply_text("Please provide a bus number")
         return
 
     # craft message
     bus_number = bus_number.upper()
     route_info = get_route_stops(bus_number, 1)
     if route_info is None:
-        await update.message.reply_text("Unknown bus number")
+        await message.reply_text("Unknown bus number")
         return
     reply_msg = bus_route_msg(bus_number, route_info)
 
     # refresh button
     reply_markup = InlineKeyboardMarkup(make_change_route_btn(bus_number, 2))
 
-    await update.message.reply_text(text=reply_msg, reply_markup=reply_markup)
+    await message.reply_text(text=reply_msg, reply_markup=reply_markup)
 
 
 async def search(
-    search_possible_stops: SearchPossibleStops, update: Update, query: list[str]
+    search_possible_stops: SearchPossibleStops, message: Message, query: list[str]
 ) -> None:
-    if update.message is None:
-        return
     possible_stops = search_possible_stops(query)
     reply_msg = bus_stop_search_msg(possible_stops)
 
-    await update.message.reply_text(text=reply_msg)
+    await message.reply_text(text=reply_msg)
 
 
-async def unknown_command(update: Update) -> None:
+async def unknown_command(message: Message) -> None:
     """
     reply user to indicate unknown command
     """
-    if update.message is None:
-        return
-    await update.message.reply_text("Unknown command")
+    await message.reply_text("Unknown command")
