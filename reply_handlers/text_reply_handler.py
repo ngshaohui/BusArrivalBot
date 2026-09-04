78757926a8d2e334152d6f70154a9b9860eb6fee
shaohui <shinlos@hotmail.com>
2026-09-05 02:50:40 +0800

    put saved_stops in user_data module

--- reply_handlers/text_reply_handler.py
+++ reply_handlers/text_reply_handler.py
@@ -10,7 +10,7 @@ from bus_service.bus_arrival import get_arriving_busses
 from bus_service.bus_stops import GetStopInfo, SearchPossibleStops
 from format_message import bus_route_msg, bus_stop_search_msg, next_bus_msg
 from reply_handlers.settings_handler import save_stop
-from saved_stops import list_saved_stops
+from user_data.saved_stops import list_saved_stops
 
 from .inline_buttons import make_change_route_btn, make_refresh_button
 
